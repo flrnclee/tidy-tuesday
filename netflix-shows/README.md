25ab3c1cd09dd6636d12a1e508afe56a078fb25f
Florence Lee <florenceclee@gmail.com>
2021-04-23 08:38:15 -0400

    Adding netflix-shows

---
+++ netflix-shows/README.md
@@ -0,0 +1,3 @@
+# tidy-tuesday-netflix
+ 
+Challenge for 2021-04-20
---
+++ netflix-shows/code/netflix_vis.R
@@ -0,0 +1,25 @@
+library(tidytuesdayR)
+library(tidyverse)
+library(DataExplorer)
+
+
+# Read in data from TidyTuesday
+tuesdata <- tidytuesdayR::tt_load('2021-04-20')
+tuesdata <- tidytuesdayR::tt_load(2021, week = 17)
+
+nfx_data <- tuesdata$netflix
+# create_report(nfx_data)
+
+str(nfx_data)
+head(nfx_data)
+
+# duration variable is combination of SEASONS and MINUTES
+# ratings have ratings for TV and for MOVIES
+
+ggplot(data=nfx_data, aes(x=rating)) +
+  geom_bar() + 
+  geom_text(stat='count', aes(label=..count..), hjust=-0.1) + 
+  scale_y_continuous(expand=c(0,0), limits=c(0, max(table(nfx_data$rating)+200)))+
+  coord_flip() 
+
+            
