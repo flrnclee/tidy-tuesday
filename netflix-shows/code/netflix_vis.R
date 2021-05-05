73687476f4b6da14e744b18023234b3d8dd194c5
Florence Lee <florenceclee@gmail.com>
2021-05-05 17:19:22 -0400

    Additional strip plots by genre & more cleaning

--- netflix-shows/code/netflix_vis.R
+++ netflix-shows/code/netflix_vis.R
@@ -26,8 +26,11 @@ head(nfx_data)
 # ratings have ratings for TV and for MOVIES
 
 nfx_mod <- nfx_data
+nfx_mod$date_added <- str_trim(nfx_mod$date_added)
 nfx_mod$date_added <- as.Date(nfx_mod$date_added, format = "%B %d, %Y")
-nfx_mod %>% filter(is.na(date_added))
+nfx_mod <- nfx_mod %>% filter(!is.na(date_added))
+
+nfx_mod$year_added <- format(nfx_mod$date_added, "%Y")
 
 ggplot(data=nfx_mod, aes(x=rating)) +
   geom_bar() + 
@@ -42,8 +45,7 @@ ggplot(data=nfx_mod, aes(x=rating)) +
 
 # Titles over time ------------------------------------------
 
-nfx_mod$year_added <- format(nfx_mod$date_added, "%Y")
-
+# By year
 ggplot(data=nfx_mod, aes(x=year_added)) +
   geom_bar() + 
   geom_text(stat='count', aes(label=..count..), hjust=-0.1) + 
@@ -60,16 +62,14 @@ title_cts <- nfx_mod %>%
   arrange(date_added_my)
 title_cts$date_added_my <- as.Date(title_cts$date_added_my)
 
-dts_all <- seq(min(nfx_mod$date_added, na.rm=T), max(nfx_mod$date_added, na.rm=T), by = "month")
+dts_mon_all <- seq(min(nfx_mod$date_added, na.rm=T), 
+                   max(nfx_mod$date_added, na.rm=T), 
+                   by = "month")
 
-dts_all <- dts_all %>% 
-  as.data.frame() %>%
-  rename("date_added_my"=".")
+title_cts <- title_cts %>% 
+  complete(date_added_my = dts_mon_all)
 
-title_cts <- dts_all %>%
-  left_join(title_cts)
-
-title_cts[is.na(title_cts)] <- 0
+title_cts[is.na(title_cts$n),]$n <- 0
 
 ggplot(data=title_cts, aes(x=date_added_my, y=n)) +
   geom_bar(stat='identity') +
@@ -80,7 +80,8 @@ ggplot(data=title_cts, aes(x=date_added_my, y=n)) +
 
 title_cts <- title_cts %>%
   mutate(date_added_my_pos = as.numeric(date_added_my)) %>%
-  select(date_added_my, date_added_my_pos, n)
+  select(date_added_my, date_added_my_pos, n) %>%
+  rename("n_all"="n")
 
 # Break axis by year based on range of dates
 
@@ -93,10 +94,10 @@ axislabels <- data.frame(label = title_cts$date_added_my[seq(1, length(title_cts
 
 # Produce strip plot
 
-stripplt <- ggplot() + geom_tile(data = title_cts,
+stripplt <- ggplot() + geom_tile(data = filter(title_cts, date_added_my >= "2015-01-01"),
                     mapping = aes(x = date_added_my_pos,
                                   y = 1,
-                                  fill = n), width = 30) +
+                                  fill = n_all), width = 30) +
   scale_fill_gradient(low =  "#190103", high ="#e50914") +
   scale_x_continuous(breaks = axisbreaks, 
                      labels = axislabels$label, 
@@ -113,6 +114,9 @@ nfx_genre <- nfx_genre %>%
   cSplit("listed_in", sep=",")
 
 clean_genre <- function(col) {
+  
+  # Function to clean and consolidate similar genres 
+  
   col <- sub("TV", "", col)
   col <- sub("Shows", "", col)
   col <- sub("Movies", "", col)
@@ -120,12 +124,17 @@ clean_genre <- function(col) {
   col <- str_trim(col)
   
   col <- ifelse(col %in% c("Classic", "Cult"), "Classic & Cult",
-                   ifelse(col=="Stand-Up Comedy & Talk", "Stand-Up Comedy",
-                          ifelse(col=="Kids'", "Kids", col)))
+                   ifelse(col %in% c("Stand-Up Comedy & Talk", "Stand-Up Comedy"), "Comedies",
+                          ifelse(col %in% c("Anime Series", "Anime Features"), "Anime",
+                                 ifelse(col=="Kids'", "Children & Family", 
+                                        ifelse(col=="Docuseries", "Documentaries",
+                                               ifelse(col %in% c("International", "British", "Spanish-Language", "Korean"), "International", col))))))
 }
 
 nfx_genre <- nfx_genre %>%
   mutate(across(c("listed_in_1", "listed_in_2", "listed_in_3"), clean_genre))
+
+# Need to deduplicate
 
 nfx_genre %>%
   select(listed_in_1, listed_in_2, listed_in_3) %>%
@@ -141,11 +150,78 @@ nfx_genre_t <- nfx_genre %>%
          listed_in_2,
          listed_in_3,
          na.rm= TRUE) %>%
-  select(-cat)
+  select(-cat) %>%
+  # There are 68 titles that had a generic genre (e.g., "Movies", "TV", or "Shows").
+  # These were excluded.
+  filter(!(genre==""))
 
-####### CHARTS #########
-# How have releases changed over time (TV Shows vs. Movies)
-# -- Number of releases
-# -- Genre of releases
+# After cleaning genres in listed_in columns, 
+# we might have duplicates by show_id and genre
+
+nfx_genre_t <- nfx_genre_t %>%
+  distinct(show_id, genre, .keep_all = TRUE)
 
 
+# Other notes --------------------------------------------
+
+# Multi-season Netflix shows are not listed individually.
+
+# "The Crown", which is currently in its 4th season, is only listed once based
+# on latest date_added.
+
+# Look at genre counts over time -------------------------
+
+unique(nfx_genre_t$genre)
+
+# How does distribution of titles change over time by genre?
+
+# If you don't normalize, you will see brighter colors over time
+# since there are more added titles.
+
+nfx_genre_cts <- nfx_genre_t %>%
+  group_by(genre) %>%
+  count(date_added_my)
+
+nfx_genre_cts$date_added_my <- as.Date(nfx_genre_cts$date_added_my)
+
+nfx_genre_cts <- nfx_genre_cts %>%
+  group_by(genre) %>%
+  complete(genre, date_added_my = dts_mon_all)
+
+nfx_genre_cts[is.na(nfx_genre_cts)] <- 0
+
+nfx_genre_cts <- nfx_genre_cts %>%
+  mutate(date_added_my_pos = as.numeric(date_added_my)) %>%
+  select(genre, date_added_my, date_added_my_pos, n) %>%
+  arrange(genre, date_added_my)
+
+# May want to limit to smaller window to avoid impact of
+# increasing num of titles being added over time
+
+ggplot() + geom_tile(data = filter(nfx_genre_cts, date_added_my > "2015-01-01", genre=="Sports"),
+                     mapping = aes(x = date_added_my_pos,
+                                   y = 1,
+                                   fill = n), width = 30) +
+  scale_fill_gradient(low =  "#190103", high ="#e50914") +
+  scale_x_continuous(breaks = axisbreaks, 
+                     labels = axislabels$label, 
+                     position = "bottom") #+ 
+#theme_void()
+
+# Normalize by total number of titles released
+
+nfx_genre_pcts <- nfx_genre_cts %>% 
+  left_join(title_cts) %>%
+  mutate(pct = 100*(n/n_all))
+
+nfx_genre_pcts[is.na(nfx_genre_pcts)] <- 0
+
+ggplot() + geom_tile(data = filter(nfx_genre_pcts, date_added_my > "2015-01-01", genre=="Documentaries"),
+                                 mapping = aes(x = date_added_my_pos,
+                                               y = 1,
+                                               fill = pct), width = 30) +
+  scale_fill_gradient(low =  "#190103", high ="#e50914", limits = c(0, 100)) +
+  scale_x_continuous(breaks = axisbreaks, 
+                     labels = axislabels$label, 
+                     position = "bottom") #+ 
+#theme_void()
